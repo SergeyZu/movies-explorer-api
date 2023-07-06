f929afd0d15e3f98ea40b5c009398863472895d3
SergeyZu <vechniystudent19@yandex.ru>
2023-07-06 14:50:39 +0700

    Настроил логирование

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
 // const { checkToken } = require('./utils/jwtAuth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect('mongodb://127.0.0.1:27017/filmsdb');
 
@@ -9,9 +10,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use(requestLogger);
+
 // app.use();
 
 app.use(router);
+
+app.use(errorLogger);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
